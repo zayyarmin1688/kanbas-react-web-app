a10c491596c302d6390e94e5472673530f136f5a
Zay Yar Min <zayarminsam1688@gmail.com>
2024-09-18 19:14:18 -0400

    Initial commit for Lab 1

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>OOGA BOOGA</h1>
+      <p>just messing with headers</p>
     </div>
   );
 }
