dccb9f36773de6ba80eded2c68670195418e6284
Zay Yar Min <zayarminsam1688@gmail.com>
2024-09-23 19:42:47 -0400

    First

---
+++ src/Kanbas/Account/index.tsx
@@ -0,0 +1,31 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import AccountNavigation from "./Navigation";
+import Profile from "./Profile";
+import Signin from "./Signin";
+import Signup from "./Signup";
+
+export default function Account() {
+  return (
+    <div id="wd-account-screen">
+      <table>
+        <tr>
+          <td valign="top">
+            <AccountNavigation />
+          </td>
+          <td valign="top">
+            <Routes>
+              <Route path="/" element={<Navigate to="/Kanbas/Account/Signin" />} />
+              <Route path="/Signin" element={<Signin />} />
+              <Route path="/Profile" element={<Profile />} />
+              <Route path="/Signup" element={<Signup />} />
+            </Routes>
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+
+
+export {}
