a78cd05e5f2887162cc7f752fe9f95200cff23e4
Zay Yar Min <68300708+zayyarmin1688@users.noreply.github.com>
2024-09-23 20:51:58 -0400

    Update TOC.tsx

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,13 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+
 export default function TOC() {
-    return (
+  return (
+    <div>
       <ul>
         <li><Link to="/Labs">Labs</Link></li>
         <li><Link to="/Labs/Lab1">Lab 1</Link></li>
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
-  </ul>
-  ); }
+      </ul>
+      {/* Zay Yar Min */}
+      <a 
+        href="https://github.com/zayyarmin1688/kanbas-react-web-app" 
+        id="wd-github"
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        View GitHub Repository
+      </a>
+    </div>
+  );}
+
 
 export {}
